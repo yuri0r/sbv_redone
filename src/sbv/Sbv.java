9e5aa42ea4e0b7062ed546b3f978f6ddb65b732b
yuri0r <macktrinz@yahoo.de>
2015-03-24 21:17:08 +0100

    changed main class back :)

--- src/sbv/Sbv.java
+++ src/sbv/Sbv.java
@@ -1,11 +1,8 @@
 package sbv;
 
-import java.util.ArrayList;
-
 public class Sbv {
 
     public static void main(String[] args) throws Exception {
-    TestQuery.Console();
-    }
-    
+    Oberflaeche.main(args);
+    }    
 }
