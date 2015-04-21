a0934f56f94babf2a9b997df62aa1f33f169c8f6
yuri0r <macktrinz@yahoo.de>
2015-04-21 21:22:44 +0200

    stuff
    stuff

--- src/sbv/Sbv.java
+++ src/sbv/Sbv.java
@@ -4,12 +4,12 @@ public class Sbv {
 
     public static void main(String[] args){
         try{
-            //Oberflaeche.main(args);
+            Oberflaeche.main(args);
 
             //Leave these methods where they are for testing purposes//////////////
             //UpdateDb.UpdateAll();
             //UpdateDb.createTables();
-            Query.Console();
+            //Query.Console();
         }catch(Exception e){System.out.println(e);}
     }    
 }
