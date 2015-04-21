00a9a5a854e801a37edf175777c59eb8fc0d0d6c
TrackmanL <l.masino@web.de>
2015-04-21 23:46:10 +0200

    Added student lists
    I made somewhat of a template for Tableupdatind within the GUI that uses
    the SQL-Querry to retrieve different Tables depending on the class.
    Wasn't able to fix BuchSuche. :(

--- src/sbv/BuchSuche.java
+++ src/sbv/BuchSuche.java
@@ -53,11 +53,11 @@ public class BuchSuche extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 709, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 585, Short.MAX_VALUE)
         );
 
         pack();
