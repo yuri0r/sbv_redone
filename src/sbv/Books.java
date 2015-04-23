672e8386b5c2e09546977060900c2051fad45adc
yuri0r <macktrinz@yahoo.de>
2015-04-23 14:42:52 +0200

    more sensfull methods names

--- src/sbv/Books.java
+++ src/sbv/Books.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Books {
     
     //Buch informationen abhängig vom der copyID und einem index  
-    public static String SingleBook ( String copyId,int index){ 
+    public static String Singlecopy ( String copyId,int index){ 
         try{
             ArrayList<String> result = Query.anyQuery("SELECT label, sbm_copieshistory.ID, distributed, collected, bought, notice, paid FROM sbm_copieshistory, sbm_copies, sbm_books, sbm_students WHERE sbm_books.ID = sbm_copies.book_id AND sbm_copieshistory.copy_id = sbm_copies.ID  AND sbm_copieshistory.student_id = sbm_students.ID AND sbm_copieshistory.ID LIKE "+copyId);
             return result.get(index);
