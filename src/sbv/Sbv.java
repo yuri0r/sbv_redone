5428aca3015413fad2b7552843818fb384cc2d03
yuri0r <macktrinz@yahoo.de>
2015-04-23 14:00:31 +0200

    nothing special

--- src/sbv/Sbv.java
+++ src/sbv/Sbv.java
@@ -3,10 +3,10 @@ package sbv;
 public class Sbv {
 
     public static void main(String[] args) throws Exception {
-    //Oberflaeche.main(args);
+    Oberflaeche.main(args);
     
     //Leave these methods where they are for testing purposes//////////////
-    UpdateDb.UpdateAll();
+    //UpdateDb.UpdateAll();
     //Query.Console();
     }    
 }
