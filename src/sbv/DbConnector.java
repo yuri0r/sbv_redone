f221d6d818feef9dd78b30ab5b2d86fb9e74f412
yuri0r <macktrinz@yahoo.de>
2015-05-07 17:37:18 +0200

    fixed "root at local host password no bug

--- src/sbv/DbConnector.java
+++ src/sbv/DbConnector.java
@@ -14,9 +14,9 @@ public class DbConnector{
         final String driver = "com.mysql.jdbc.Driver";                //chosing driver
         final String url = "jdbc:mysql://localhost:3306/sbv_aes_2013";//choosing mySQL server server
         final String username = "root";                               //DB ussername and password
-        final String password = "";
+        final String password = "usbw";
         Class.forName(driver); 
-        Connection con = DriverManager.getConnection(url,username­,password); //Connecting
+        Connection con = DriverManager.getConnection(url,username,password); //Connecting
         System.out.println("Connected");
         return con;
         } 
