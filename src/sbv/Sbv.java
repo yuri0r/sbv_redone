37805911276d40fe7eb0c7dc52093a866297d3af
yuri0r <macktrinz@yahoo.de>
2015-10-01 21:13:38 +0200

    stupid paid
    removed the paid statement

--- src/sbv/Sbv.java
+++ src/sbv/Sbv.java
@@ -3,17 +3,20 @@ package sbv;
 public class Sbv {
 
     public static void main(String[] args) throws Exception {
-        Copies.copyBill("234");
-        //UpdateDb.updateDPrice();
-        //UpdateDb.massAddClasses(328, 350, 14);
-        //Students.newStudent("max", "mustermann", "1999-10-05");
-        Oberflaeche.main(args);
-    
+
+
+        //Oberflaeche.main(args);
+            
     //Leave these methods where they are for testing purposes//////////////
     //UpdateDb.UpdateAll();
     //Query.Console();
-    //    Oberflaeche ob = new Oberflaeche();
+    Oberflaeche ob = new Oberflaeche();
     //PDF_Export.bestandPDF(ob);
+    PDF_Export.studentBill("216", ob);    
+    Copies.copyBill("216");
+        //UpdateDb.updateDPrice();
+        //UpdateDb.massAddClasses(328, 350, 14);
+        //Students.newStudent("max", "mustermann", "1999-10-05");
    /*System.out.println(Copies.SingleCopyCountTotal("1"));
       System.out.println(Copies.boughtCopyCount("1"));
          System.out.println(Copies.borrowedCopyCount("1"));
